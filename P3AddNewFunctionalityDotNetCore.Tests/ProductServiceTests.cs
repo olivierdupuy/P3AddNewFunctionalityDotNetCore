bec6ef5f5c177e66548ad492826a786418ab80d1
Olivier Dupuy <olivier.dupuy74@hotmail.fr>
2023-12-07 18:53:02 +0100

    Maj develop

--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -12,22 +12,6 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
-        {
-            // Arrange
-
-            // Act
-
-
-            // Assert
-            Assert.Equal(1, 1);
-        }
 
         static List<string> validityResult(ProductViewModel product)
         {
